130c4be8ac52008c4731286f7e209976cc16312b
Miguerubsk <miguerubsk@gmail.com>
2024-05-14 16:54:57 +0100

    Added IDs for every component

--- src/geneticAlg/codeHelper.java
+++ src/geneticAlg/codeHelper.java
@@ -16,6 +16,42 @@
  */
 package geneticAlg;
 
+import Simulator.AdvancedHeatExchanger;
+import Simulator.AdvancedHeatVent;
+import Simulator.ComponentHeatExchanger;
+import Simulator.ComponentHeatVent;
+import Simulator.ContainmentReactorPlating;
+import Simulator.CoolantCell10k;
+import Simulator.CoolantCell180kHelium;
+import Simulator.CoolantCell180kNak;
+import Simulator.CoolantCell30k;
+import Simulator.CoolantCell360kHelium;
+import Simulator.CoolantCell360kNak;
+import Simulator.CoolantCell60k;
+import Simulator.CoolantCell60kHelium;
+import Simulator.CoolantCell60kNak;
+import Simulator.DualFuelRodMox;
+import Simulator.DualFuelRodThorium;
+import Simulator.DualFuelRodUranium;
+import Simulator.FuelRodMox;
+import Simulator.FuelRodThorium;
+import Simulator.FuelRodUranium;
+import Simulator.HeatCapacityReactorPlating;
+import Simulator.HeatExchanger;
+import Simulator.HeatVent;
+import Simulator.IridiumNeutronReflector;
+import Simulator.LzhCondensator;
+import Simulator.NeutronReflector;
+import Simulator.OverclockedHeatVent;
+import Simulator.QuadFuelRodMox;
+import Simulator.QuadFuelRodThorium;
+import Simulator.QuadFuelRodUranium;
+import Simulator.ReactorHeatExchanger;
+import Simulator.ReactorHeatVent;
+import Simulator.ReactorPlating;
+import Simulator.RshCondensator;
+import Simulator.ThickNeutronReflector;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -25,19 +61,68 @@ import java.util.Random;
 public class codeHelper {
 
     Random rnd = new Random(System.currentTimeMillis());
-    public final String[] validIDs = {"1B", "0D", "11", "14"};
+    private static final Object[][] components = {
+        {"empty", null},
+        {"fuelRodUranium", new FuelRodUranium()},
+        {"dualFuelRodUranium", new DualFuelRodUranium()},
+        {"quadFuelRodUranium", new QuadFuelRodUranium()},
+        {"fuelRodMox", new FuelRodMox()},
+        {"dualFuelRodMox", new DualFuelRodMox()},
+        {"quadFuelRodMox", new QuadFuelRodMox()},
+        {"neutronReflector", new NeutronReflector()},
+        {"thickNeutronReflector", new ThickNeutronReflector()},
+        {"heatVent", new HeatVent()},
+        {"advancedHeatVent", new AdvancedHeatVent()},
+        {"reactorHeatVent", new ReactorHeatVent()},
+        {"componentHeatVent", new ComponentHeatVent()},
+        {"overclockedHeatVent", new OverclockedHeatVent()},
+        {"coolantCell10k", new CoolantCell10k()},
+        {"coolantCell30k", new CoolantCell30k()},
+        {"coolantCell60k", new CoolantCell60k()},
+        {"heatExchanger", new HeatExchanger()},
+        {"advancedHeatExchanger", new AdvancedHeatExchanger()},
+        {"coreHeatExchanger", new ReactorHeatExchanger()},
+        {"componentHeatExchanger", new ComponentHeatExchanger()},
+        {"reactorPlating", new ReactorPlating()},
+        {"heatCapacityReactorPlating", new HeatCapacityReactorPlating()},
+        {"containmentReactorPlating", new ContainmentReactorPlating()},
+        {"rshCondensator", new RshCondensator()},
+        {"lzhCondensator", new LzhCondensator()},
+        {"fuelRodThorium", new FuelRodThorium()},
+        {"dualFuelRodThorium", new DualFuelRodThorium()},
+        {"quadFuelRodThorium", new QuadFuelRodThorium()},
+        {"coolantCellHelium60k", new CoolantCell60kHelium()},
+        {"coolantCellHelium180k", new CoolantCell180kHelium()},
+        {"coolantCellHelium360k", new CoolantCell360kHelium()},
+        {"coolantCellNak60k", new CoolantCell60kNak()},
+        {"coolantCellNak180k", new CoolantCell180kNak()},
+        {"coolantCellNak360k", new CoolantCell360kNak()},
+        {"iridiumNeutronReflector", new IridiumNeutronReflector()},};
+
+    private ArrayList<String> createIDs() {
+        
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 1; i < components.length; i++) {
+
+            result.add(String.format("%02X", i));
+
+        }
+        return result;
+    }
+
+    public final ArrayList<String> validIDs = createIDs();
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public String getRandomID() {
-        return validIDs[rnd.nextInt(validIDs.length)];
+        return validIDs.get(rnd.nextInt(validIDs.size()));
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public String getRandomCode() {
         StringBuilder sb = new StringBuilder(108);
@@ -48,9 +133,9 @@ public class codeHelper {
     }
 
     /**
-     * 
+     *
      * @param code
-     * @return 
+     * @return
      */
     public String mutateGene(String code) {
         StringBuilder sb = new StringBuilder();
@@ -68,10 +153,10 @@ public class codeHelper {
     }
 
     /**
-     * 
+     *
      * @param code1
      * @param code2
-     * @return 
+     * @return
      */
     public String onePointCrossover(String code1, String code2) {
         int pos = rnd.nextInt(53) * 2;
@@ -85,35 +170,35 @@ public class codeHelper {
         }
         return sb.toString();
     }
-    
+
     /**
-     * 
+     *
      * @param code1
      * @param code2
-     * @return 
+     * @return
      */
     public String twoPointCrossover(String code1, String code2) {
         int pos1 = rnd.nextInt(53) * 2;
         int pos2 = rnd.nextInt(53) * 2;
-        
-        if(pos2 < pos1){
+
+        if (pos2 < pos1) {
             int aux = pos1;
             pos1 = pos2;
             pos2 = aux;
         }
-        
+
         StringBuilder sb = new StringBuilder(108);
-        
+
         for (int i = 0; i < 108; i++) {
             if (i >= pos1 && i <= pos2) {
                 sb.append(code2.charAt(i));
             }
-            
-            if (i < pos1){
+
+            if (i < pos1) {
                 sb.append(code1.charAt(i));
             }
-            
-            if(i > pos2){
+
+            if (i > pos2) {
                 sb.append(code2.charAt(i));
             }
         }
